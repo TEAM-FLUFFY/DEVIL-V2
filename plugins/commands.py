1b5d8cbe905b333df1008547d48616eca524b913
TEAM-FLUFFY <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-07 17:43:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,7 +40,7 @@ async def start(client, message):
             InlineKeyboardButton('⚡𝗛𝗘𝗟𝗣⚡', callback_data='help'),
             InlineKeyboardButton('🕵️‍♂️𝗦𝗘𝗔𝗥𝗖𝗛🕵️‍♂️', switch_inline_query_current_chat=''),
             ],[
-            InlineKeyboardButton('⭐𝗟𝗘𝗧'𝗦 𝗥𝗢𝗟𝗟⭐', url='http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🕯️𝗔𝗗𝗗 𝗠𝗘', url='http://t.me/{temp.U_NAME}?startgroup=true')
             
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
